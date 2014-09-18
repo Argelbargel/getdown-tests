fadc45b65a022b16e8451b3b476bbb0423f6ba9e
Argelbargel <karsten.kraus@web.de>
2014-09-18 16:39:57 +0200

    integrated Test-Application  (formerly getdown-test) into this repository

---
+++ getdown-testapp/src/main/java/de/theprojects/getdown/testapp/Main.java
@@ -0,0 +1,12 @@
+package de.theprojects.getdown.testapp;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main  {
+
+    public static void main(String[] args) {
+        System.out.println("testapp running. All is well!");
+        System.exit(0);
+    }
+}
